7bec12b51be43f73d52d3c59cfa194cf1272a887
C. Lewis <ctj.lewis@icloud.com>
2022-11-21 20:43:44 -0600

    v0.0.3

--- src/lib/test.ts
+++ src/lib/test.ts
@@ -2,14 +2,22 @@ import { SpinnerResult, spinners } from "@tsmodule/spinners";
 import { TestFn } from "../types";
 import { printResult } from "./print";
 
+export type TestOptions = {
+  flushStdout?: boolean;
+};
+
 export const test = async (
   name: string,
   testFn: TestFn,
+  { flushStdout = true }: TestOptions = {}
 ) => {
   try {
-    const spinnerResult = await spinners({
-      [name]: async () => await testFn()
-    });
+    const spinnerResult = await spinners(
+      {
+        [name]: async () => await testFn()
+      },
+      { flushStdout }
+    );
 
     printResult(spinnerResult);
     return spinnerResult;
@@ -21,9 +29,14 @@ export const test = async (
 
 export const tests = async (
   testConfigs: Record<string, TestFn>,
+  { flushStdout = true }: TestOptions = {}
 ) => {
   try {
-    const spinnerResult = await spinners(testConfigs);
+    const spinnerResult = await spinners(
+      testConfigs,
+      { flushStdout }
+    );
+
     return spinnerResult;
   } catch (spinnerResult) {
     printResult(spinnerResult as SpinnerResult);
